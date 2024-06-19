864bbe634b922ef3a7be71d8ea217364188731ac
APapeIsName <gms72901217@gmail.com>
2024-06-19 15:41:57 +0900

    240619 ver 0.1.0 Updated. SignInActivity Base Updated

---
+++ app/src/main/java/com/android/nbc_login/HomeActivity.kt
@@ -0,0 +1,24 @@
+package com.android.nbc_login
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val loginId = intent.getStringExtra("SignIn")
+        val tv = findViewById<TextView>(R.id.tv_test)
+        tv.setText("테스트 : $loginId")
+    }
+}
---
+++ app/src/main/java/com/android/nbc_login/SignInActivity.kt
@@ -0,0 +1,46 @@
+package com.android.nbc_login
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignInActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_signin)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+        val id = findViewById<EditText>(R.id.et_id)
+        val pwd = findViewById<EditText>(R.id.et_pwd)
+        btnLogin.setOnClickListener {
+            if(id.text.toString().isBlank() || pwd.text.toString().isBlank()) {
+                Toast.makeText(this, "ID/비밀번호를 전부 입력해주세요.", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "로그인 성공!", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, HomeActivity::class.java)
+                intent.putExtra("SignIn", id.text.toString())
+                startActivity(intent)
+            }
+        }
+        val btnSignUp = findViewById<Button>(R.id.btn_register)
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
